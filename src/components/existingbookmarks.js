65a72ecf13f4a4bbf83582693e8488efb4954270
julian so <julianso89@gmail.com>
2018-11-12 17:46:17 -0800

    existing bookmarks in progress

---
+++ src/components/existingbookmarks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DummyList from '/dummylist';
+
+class existingBookmarks extends Component {
+
+    render(){ 
+        return ( 
+            <ul>
+                <li> Existing Bookmarks </li>
+            </ul>
+         );
+    }
+}
+ 
+export default existingBookmarks;
