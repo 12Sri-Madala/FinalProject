40366ac5492955a2f4b3da7e781c8caae5e66213
julian so <julianso89@gmail.com>
2018-11-19 17:37:51 -0800

    existing bookmarks2 updated

--- src/components/application_page.js
+++ src/components/application_page.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReminderList from './reminder_list';
 import ExistingBookmarks from './existingbookmarks';
+import ExistingFolders from './existingfolders';
 
 
 const ApplicationPage = () => (
             <div>
-                <h1>APPLICATION PAGE</h1>
-                <ReminderList />
+                <h1>Application Page</h1>
+                <ReminderList/>
+                <h1>Bookmarks</h1>
                 <ExistingBookmarks/>
+                <h1>Folders</h1>
+                <ExistingFolders/>
             </div>
 )
 
---
+++ src/existingdummy/dummyfolders.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default [
+    {
+        folder: 'Shopping',
+        _id: '001'
+    },
+    {
+        folder: 'School',
+        _id: '002'
+    },
+    {
+        folder: 'Sports',
+        _id: '003'
+    },
+    {
+        folder: 'Videos',
+        _id: '004'
+    }
+];
