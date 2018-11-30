0f5b4efc804940a1c5d84a568d30e009db41ba4e
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-29 16:42:12 -0800

    background and team-member component started

---
+++ client/src/components/team_page/team_member.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blank from './images/blank-member.png';
+
+
+const TeamMember = () => {
+    return (
+        <div className="member-container">
+            <img src={Blank} />
+        </div>
+    )
+}
+
+export default TeamMember;
