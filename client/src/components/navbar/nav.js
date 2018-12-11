73531a9027c38e488d3018caa03c2932be8660d7
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-12-10 16:27:16 -0800

    made changes to nav look and location

--- client/src/components/navbar/nav.js
+++ client/src/components/navbar/nav.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './nav.css';
 
 export default props => (
-         <nav>
-         <div class="nav-wrapper">
-           <a href="#" class="brand-logo">Crease</a>
-           <ul id="nav-mobile" class="right hide-on-med-and-down">
-             <li>
-                <NavLink exact to={'/'} activeClassName="active selected" className="nav-link">Home</NavLink>
-             </li>
-             <li>
-                <NavLink to={'/application_page'} activeClassName="active selected" className="nav-link">Application</NavLink>
-             </li>
-             <li>
-                <NavLink to={'/meet_team'} activeClassName="active selected" className="nav-link">Meet The Team</NavLink>
-             </li>
-           </ul>
+      <nav id="navbar">
+         <div className="nav-wrapper main-nav">
+           <Link className="brand-logo nav-logo" to="/application_page">Crease</Link>
+            <ul id="nav-mobile" className="right">
+               <li>
+                  <NavLink to={'/application_page'} activeClassName="active selected" className="nav-link">Application</NavLink>
+               </li>
+               <li>
+                  <NavLink to={'/meet_team'} activeClassName="active selected" className="nav-link">Meet The Team</NavLink>
+               </li>
+               <li>
+                  <button className="red btn darken-2">Sign Out</button>
+               </li>
+            </ul>
          </div>
-       </nav>
+      </nav>
 );
