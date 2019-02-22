d9767da6a6618351975dce2ea83b2710da4fb5f8
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-21 16:39:44 -0800

    ipad pro homepage media query done

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -27,13 +27,21 @@ export default props => {
                 <div  className="crease-logo"></div>
 
                 <div className="login-area">
-                    <a href='auth/google'><button className='login-button' type='button'>LOGIN</button></a>
-                    <a href='auth/google'><button className='login-button' type='button'>SIGNUP</button></a>
-      
+                    <div className="login-button-container">
+                        <a href='http://localhost:8000/auth/google'><button className='login-button' type='button'>LOGIN</button></a>
+                        <a href='http://localhost:8000/auth/google'><button className='login-button' type='button'>SIGNUP</button></a>
+                    </div>
+                    <div className="device-message-container">
+                        <p><i class="fas fa-exclamation-triangle"></i> Friendly Reminder <i class="fas fa-exclamation-triangle"></i></p>
+                        <span class="device-message">
+                            This application is built as a chrome extension which is currently incompatible with mobile devices.
+                            Please download our application on your laptop or desktop to access our features.
+                        </span>
+                    </div>
                 </div>
 
                 <div className="login-area">
-                    <a href='http://localhost:3000/teampage'><button className="login-button">MEET THE TEAM</button></a>
+                    <a href='http://localhost:3000/teampage'><button className="login-button team-button">MEET THE TEAM</button></a>
                 </div>
             </div>
             
@@ -41,32 +49,29 @@ export default props => {
                 <div className="description-header">
                     <h3 className="center description-title">What is <span className="lobster">Crease</span> ?</h3>
 
-                    <h6 className="center description-title"><span className="lobster">Crease</span> is an all-in-one bookmark manager for your inspirations and read laters</h6>
+                    <h6 className="center description-subtitle"><span className="lobster">Crease</span> is an all-in-one bookmark manager for your inspirations and read laters</h6>
                 </div>
 
                 <div className="container video-container-parent">
 
                     <div className="row">
                         <div className="organize col s6 description-title">Organize and view with ease</div>
-                        <div className="set col s4 offset-s2 description-title">Set a <span className="lobster">Crease</span> reminder</div>
+                        <div className="set col s6 description-title">Set a <span className="lobster">Crease</span> reminder</div>
                     </div>
 
-                    <div className="row">
+                    <div className="videos">
                         
-                        <div className="col s5">
+                        <div className="col s5 organize-video">
                             <iframe width="420" height="345" src="https://www.youtube.com/embed/S-AT4nrDjgs?autoplay=1&loop=1&playlist=S-AT4nrDjgs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>                        
                         </div>
 
-                        <div className="col s5 offset-s2">
+                        <div className="col s5 offset-s2 set-reminder-video">
                             <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&loop=1&playlist=tgbNymZ7vqY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>                        
                         </div>
                     
                     </div>
                     
                 </div>
-                    <div>
-                        <h1>Technologies Used:</h1>
-                    </div>
             </div>
         </div>
     )
